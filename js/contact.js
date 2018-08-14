5c481fea94b8dfa906875081d8087d78997c18fe
Sam Ilkov <samuil.ilkov90@gmail.com>
2018-08-13 21:44:50 -0400

    added Firebase for messages

---
+++ js/contact.js
@@ -0,0 +1,41 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB6jU19gZD8jTOSPfATmkpw7YPbJAFyyGM",
+    authDomain: "mywebsite-47249.firebaseapp.com",
+    databaseURL: "https://mywebsite-47249.firebaseio.com",
+    projectId: "mywebsite-47249",
+    storageBucket: "",
+    messagingSenderId: "756088636112"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database(); 
+
+$("#submit").on("click", function (event) {
+
+    event.preventDefault();
+    var contactName = $("#name").val().trim();
+    var contactEmail = $("#exampleInputEmail").val().trim();
+    var contactPhone = $("#telephone").val().trim();
+    var contactMessage = $("#description").val().trim();
+
+
+    alert("Thank you for getting in touch! I'll get back with you shortly.");
+
+    var newContact = {
+        name: contactName,
+        email: contactEmail,
+        telephone: contactPhone,
+        message: contactMessage
+    };
+
+    database.ref().push(newContact);
+    $("#name").val("");
+    $("#exampleInputEmail").val("");
+    $("#telephone").val("");
+    $("#description").val("");
+
+});
+
